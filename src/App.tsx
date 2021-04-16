9d1c05be30a90906a5fa1ed8c301e2d05736060f
Jack Palfrey <jackgpalfrey@outlook.com>
2021-04-16 10:55:33 +0100

    Added basic grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const numRows = 60;
+const numCols = 30;
+
+const App: React.FC = () => {
+	const [grid, setGrid] = useState(createGrid());
+
+	function createGrid() {
+		let newArr = [];
+		for (let i = 0; i < numRows; i++) {
+			let row = [];
+			for (let j = 0; j < numCols; j++) {
+				row.push(false);
+			}
+			newArr.push(row);
+		}
+		return newArr;
+	}
+
+	function setNode(x: number, y: number, isActive: boolean) {
+		let newGrid = [...grid];
+		newGrid[x][y] = isActive;
+		setGrid(newGrid);
+	}
+
+	function drawGrid() {
+		let divGrid = grid.map((row, x) => {
+			let colDivs = row.map((node, y) => {
+				return (
+					<div
+						style={{
+							width: '20px',
+							height: '20px',
+							backgroundColor: `${node ? 'red' : null}`,
+							border: 'solid 1px black',
+						}}
+						onClick={() => {
+							setNode(x, y, !node);
+						}}
+					/>
+				);
+			});
+			return (
+				<div style={{ display: 'flex', flexDirection: 'column' }}>
+					{colDivs}
+				</div>
+			);
+		});
+
+		return divGrid;
+	}
+
+	console.log(grid);
+	return (
+		<div
+			className='App'
+			style={{
+				display: 'flex',
+				flexDirection: 'row',
+			}}>
+			{drawGrid()}
+		</div>
+	);
+};
 
 export default App;
